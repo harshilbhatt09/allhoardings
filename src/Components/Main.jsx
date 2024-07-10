2a03d6897c7ad36e712b8bc8d68ae828c3fb9a15
harshilbhatt09 <bhattharshu9503@gmail.com>
2024-07-10 17:59:31 +0530

    Dynamic cards

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
 
 function Main() {
+  let card = 6;
+  let cardList = [];
+  for (let i = 1; i <= card; i++) {
+    cardList.push(
+      <div>
+        <Card />
+      </div>
+    );
+  }
   return (
-    <div className='font-[Raleway] py-7'>
-        <div className='flex justify-center font-black text-xl md:text-3xl'>
-          Featured hoardings
-        </div>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-5 md:py-9'>
-            <div><Card/></div>
-            <div><Card/></div>
-            <div><Card/></div>
-            <div><Card/></div>
-            <div><Card/></div>
-            <div><Card/></div>
-        </div>
-        <div className='flex justify-center py-3'>
-          <button className='border-2 px-3 border-black rounded-full text-xs md:text-base font-bold bg-[#F8FBFA]'>View all</button>
-        </div>
+    <div className="font-[Raleway] py-7">
+      <div className="flex justify-center font-black text-xl md:text-3xl">
+        Featured hoardings
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-5 md:py-9">
+        {cardList}
+      </div>
+      <div className="flex justify-center py-3">
+        <button className="border-2 px-3 border-black rounded-full text-xs md:text-base font-bold bg-[#F8FBFA]">
+          View all
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
